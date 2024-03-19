3710c62f6f7cb20cc623b9ca50c995490de8a14b
charitha86 <162983741+charitha86@users.noreply.github.com>
2024-03-19 11:05:13 +0530

    Add files via upload

---
+++ python day7.py
@@ -0,0 +1,106 @@
+# 2.)python program to determine if
+# the given sets are disjointt
+# or not without using Inbuilt
+
+# set1 = {'python', 'java', 'Data science'}
+# set2 = {'Ml', 'AI', 'R Language', 'Python'}
+# set3 = {'Data Analytics', 'Robotics', 'Deep Learing'}
+# c = 0
+# flag=0
+#  for val in range(3):
+#    c=c+1
+#    if c==1:
+#        for val in set1:
+#            if val in set2 or val in set3:
+#                flag=1
+#                break
+#    if c==2:
+#        for val in set2:
+#            if val in set1 or val in set3:
+#                flag=1
+#                break
+                
+#    if c==3:
+#        for val in set3:
+#            if val in set2 or val in set1:
+#                flag=1
+#                break
+
+# if flag==0:
+#    print("Disjoint")
+# else:
+#    print("joint")
+
+# 3.)
+# list1 = ["M", "na", "i", "ke"]
+# list2 = ["y", "me", "s", "lly"]
+
+# 13 = []
+# for val in range()len(list1)):
+#    ans = list1[val]+list2[val]
+#    l3.append(ans)
+# print(l3)
+# ? or
+# i=0
+#while i<len(list1):
+#    l3.append(list1[i]+list2[i])
+#    i+=1
+# print(l3)
+
+# ! Funtions
+#? DEF
+# Functions is a block of code which is used to perform a specific functionality
+#? Funtions can be created using def keyword
+
+# ? Functions has 3 parts
+# Functions declaration
+# Functions defanition
+# Function call
+# Eg:1
+# def greet(): # function defination 
+#    print("Greetings user!!")
+# greet()
+# greet()
+# greet()
+# greet()
+# greet()
+# greet() # function calling
+
+# ! Eg:2
+# ? Function with parameter
+def greeting(name): # name is parameter
+    print("Welcome", name)
+
+for val in range(3):
+    username = input("Enter the name: ")
+    greeting(username) # username is argument
+# ! Eg:3
+# 1.) Write a python script to generate and print a dictionary that
+# contains a number (between 1 and n) in the form (x, x*x).
+# sample Dictionary (n = 5):
+# Expected output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+# 2.)Find thr uncommon words from 2 strings
+    
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
